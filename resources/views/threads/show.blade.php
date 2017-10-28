8b576bfc84ad9a824952821e5d4d168992a5e545
roerjo <roerjo.personal@gmail.com>
2017-10-28 19:53:03 -0400

    Add reply form on indiviual thread

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -33,5 +33,18 @@
     </div>
   </div>
 
+  @if (auth()->check())
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <form method="POST" action="/threads/{{ $thread->id }}/replies">
+          {{ csrf_field() }}
+          <div class="form-group">
+            <textarea name="body" id="body" class="form-control" placeholder="Add a reply..." rows="5"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  @endif
 </div>
 @endsection
