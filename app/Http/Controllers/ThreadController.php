191c5a45b23b23ffd8f042197de203326cb856c4
roerjo <roerjo.personal@gmail.com>
2017-11-01 15:14:12 -0400

    create thread for auth user passing

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class ThreadController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->only('store');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -37,6 +42,13 @@ class ThreadController extends Controller
      */
     public function store(Request $request)
     {
+        $thread = Thread::create([
+            'title' => $request->title,
+            'body' => $request->body,
+            'user_id' => auth()->id(),
+        ]);
+
+        return redirect('threads/' . $thread->id);
     }
 
     /**
